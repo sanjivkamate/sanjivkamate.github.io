44c48226f1d4df8760faa7822cdd2715bc5f46aa
Sanjiv Kamate <sanjiv15690@gmail.com>
2019-06-06 15:00:35 +0200

    Added terms of use pdf

--- vendor/bootstrap/js/custom.js
+++ vendor/bootstrap/js/custom.js
@@ -2,3 +2,16 @@
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+/*added by sanjiv for navbar navigation*/
+$(function(){
+	var navbar = $('.navbar');
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() <= 40){
+			navbar.removeClass('navbar-scroll');
+		} else {
+			navbar.addClass('navbar-scroll');
+		}
+	});
+});
